664e2aa41658ccb6c7fe2467c8767fc196de2af3
kunisada <kunisada@ryukyu-i.co.jp>
2017-04-12 12:59:12 +0900

    [modify] smalls into capital letters

--- kadai_vol2/kadai.sql
+++ kadai_vol2/kadai.sql
@@ -1,16 +1,16 @@
 -- 課題_第2弾の課題1~18
 
 # 課題1
-SELECT * FROM users WHERE area="沖縄県" and gender = "女";
+SELECT * FROM users WHERE area="沖縄県" AND gender = "女";
 
 # 課題2
-SELECT name,email FROM users WHERE area="千葉県" and gender = "男";
+SELECT name,email FROM users WHERE area="千葉県" AND gender = "男";
 
 # 課題3
-SELECT * FROM items ORDER BY price desc;
+SELECT * FROM items ORDER BY price DESC;
 
 # 課題4
-SELECT * FROM items ORDER BY price desc limit 0,10;
+SELECT * FROM items ORDER BY price DESC LIMIT 0,10;
 
 # 課題5
 SELECT * FROM items WHERE stock >= 100;
@@ -22,16 +22,16 @@ SELECT * FROM items WHERE stock BETWEEN 40 AND 100;
 SELECT * FROM users WHERE area="愛知県" OR area="大阪府";
 
 # 課題8
-SELECT * FROM users WHERE name like "%島%";
+SELECT * FROM users WHERE name LIKE "%島%";
 
 # 課題9
-SELECT * FROM users WHERE name like "大%";
+SELECT * FROM users WHERE name LIKE "大%";
 
 # 課題10
-SELECT * FROM users WHERE name like "%郎";
+SELECT * FROM users WHERE name LIKE "%郎";
 
 # 課題11
-SELECT * FROM users WHERE name like "%田%" OR area like "%都";
+SELECT * FROM users WHERE name LIKE "%田%" OR area LIKE "%都";
 
 # 課題12
 INSERT INTO items VALUES (1015,"ご家庭用宮古島完熟マンゴー 1kg","沖縄特産品本舗",4180,20);
@@ -49,7 +49,7 @@ DELETE FROM items WHERE id = 1015 OR id = 1016;
 SELECT maker,SUM(stock) FROM items GROUP BY maker;
 
 # 課題17
-SELECT maker,SUM(stock) FROM items GROUP BY maker HAVING Sum(stock) >= 200;
+SELECT maker,SUM(stock) FROM items GROUP BY maker HAVING SUM(stock) >= 200;
 
 # 課題18
 SELECT * FROM users JOIN orders ON users.id = orders.user_id;
