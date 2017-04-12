d00311f74a87222aafd6b397ea7f53dc7da5cb5a
kunisada <kunisada@ryukyu-i.co.jp>
2017-04-12 13:48:30 +0900

    [fixed] for issue#5

--- kadai_vol1/kadai_09.php
+++ kadai_vol1/kadai_09.php
@@ -12,4 +12,6 @@
 	echo "今日は{$lunch_price2}円（税抜）の弁当を{$lunch_count2}個食べました。";
 	echo "<br />";
 	echo "今日の弁当代は".intval($lunch_price2*$lunch_count2*$tax)."円（税込）です。";
+	echo "<br />";
+	echo "昨日と今日で".intval(($lunch_price1*$lunch_count1+$lunch_price2*$lunch_count2)*$tax)."円(税込)使いました。";
 ?>
